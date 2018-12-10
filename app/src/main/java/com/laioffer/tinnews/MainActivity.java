59255f495ccd9ed3fda00f4e199162f7e14f6db7
Jingyang Zhang <jingyangzhang@10-18-1-30.dynapool.wireless.nyu.edu>
2018-12-09 19:14:40 -0500

    1209

---
+++ app/src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -0,0 +1,24 @@
+package com.laioffer.tinnews;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
